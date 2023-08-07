9fdd8f635db46607ea7bad127aae58c026b58e70
Sarah <75456177+SarahW2226@users.noreply.github.com>
2023-08-07 13:09:47 -0700

    Create temp-2b.py

---
+++ temp-2b.py
@@ -0,0 +1,5 @@
+#Program that converts Celsius to Fahrenheit
+celsius = float(input("Please enter a Celsius temperature. \n"))
+faren = (9/5)*celsius + 32
+print("The equivalent Fahrenheit temperature is: ")
+print(faren)
